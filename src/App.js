e33e0065b621ff18ea00b438f4250fdc8058af57
Douglas Hennrich <douglas_b_h@hotmail.com>
2020-06-26 18:37:50 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,107 +1,130 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 
 import {
-  SafeAreaView,
-  View,
-  FlatList,
-  Text,
-  StatusBar,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+	SafeAreaView,
+	View,
+	FlatList,
+	Text,
+	StatusBar,
+	StyleSheet,
+	TouchableOpacity,
+} from 'react-native'
+
+import axios from './services/api'
 
 export default function App() {
-  async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
-  }
+	const [repositories, setRepositores] = useState([])
 
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+	useEffect(() => {
+		axios.get('repositories').then(res => {
+			const { data } = res
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+			setRepositores(data)
+		})
+	}, [])
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+	async function handleLikeRepository(id) {
+		// Implement "Like Repository" functionality
+		const { data: repository } = await axios.post(`repositories/${id}/like`)
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+		const index = repositories.findIndex(curr => curr.id === id)
+
+		if (index < 0) {
+			return
+		}
+
+		const mapped = [...repositories]
+
+		mapped[index] = repository
+
+		setRepositores(mapped)
+	}
+
+	return (
+		<>
+			<StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+			<SafeAreaView style={styles.container}>
+				<FlatList
+					data={repositories}
+					keyExtractor={repository => repository.id}
+					renderItem={({ item: repository }) => (
+						<View style={styles.repositoryContainer}>
+							<Text style={styles.repository}>{repository.title}</Text>
+
+							<View style={styles.techsContainer}>
+								{repository.techs.map((tech, index) => (
+									<Text key={index}>{tech}</Text>
+								))}
+							</View>
+
+							<View style={styles.likesContainer}>
+								<Text style={styles.likeText} testID={`repository-likes-${repository.id}`}>
+									{repository.likes} curtidas
+								</Text>
+							</View>
+
+							<TouchableOpacity
+								style={styles.button}
+								onPress={() => handleLikeRepository(repository.id)}
+								testID={`like-button-${repository.id}`}
+							>
+								<Text style={styles.buttonText}>Curtir</Text>
+							</TouchableOpacity>
+						</View>
+					)}
+				/>
+			</SafeAreaView>
+		</>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#7159c1",
-  },
-  repositoryContainer: {
-    marginBottom: 15,
-    marginHorizontal: 15,
-    backgroundColor: "#fff",
-    padding: 20,
-  },
-  repository: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-  techsContainer: {
-    flexDirection: "row",
-    marginTop: 10,
-  },
-  tech: {
-    fontSize: 12,
-    fontWeight: "bold",
-    marginRight: 10,
-    backgroundColor: "#04d361",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    color: "#fff",
-  },
-  likesContainer: {
-    marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  likeText: {
-    fontSize: 14,
-    fontWeight: "bold",
-    marginRight: 10,
-  },
-  button: {
-    marginTop: 10,
-  },
-  buttonText: {
-    fontSize: 14,
-    fontWeight: "bold",
-    marginRight: 10,
-    color: "#fff",
-    backgroundColor: "#7159c1",
-    padding: 15,
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#7159c1',
+	},
+	repositoryContainer: {
+		marginBottom: 15,
+		marginHorizontal: 15,
+		backgroundColor: '#fff',
+		padding: 20,
+	},
+	repository: {
+		fontSize: 32,
+		fontWeight: 'bold',
+	},
+	techsContainer: {
+		flexDirection: 'row',
+		marginTop: 10,
+	},
+	tech: {
+		fontSize: 12,
+		fontWeight: 'bold',
+		marginRight: 10,
+		backgroundColor: '#04d361',
+		paddingHorizontal: 10,
+		paddingVertical: 5,
+		color: '#fff',
+	},
+	likesContainer: {
+		marginTop: 15,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	likeText: {
+		fontSize: 14,
+		fontWeight: 'bold',
+		marginRight: 10,
+	},
+	button: {
+		marginTop: 10,
+	},
+	buttonText: {
+		fontSize: 14,
+		fontWeight: 'bold',
+		marginRight: 10,
+		color: '#fff',
+		backgroundColor: '#7159c1',
+		padding: 15,
+	},
+})
